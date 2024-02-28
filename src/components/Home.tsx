0084493765553d514a7380e9eb746cd6a960d0f3
Krazza <muondove@gmail.com>
2024-02-28 15:59:39 +0200

    horizontal scroll first iteration

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+import Portfolio from "./Portfolio";
 import "../styles/Home.css";
 
-import Portfolio from "./Portfolio";
-
 function Home () {
+
+    const targetRef = useRef<HTMLDivElement | null>(null);
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+    });
+
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"])
+
     return(
-        <main className="homeContainer">
-            <h1>{"Which portfolio interests you?"}</h1>
-            <div className="menuContainer">
-                <section>
-                    <h2>{"web"}</h2>
-                </section>
-                <section>
-                    <h2>{"game"}</h2>
-                </section>
-            </div>
-            <Portfolio/>
+        <main ref={targetRef} className="homeContainer">
+            <motion.div style={{ x }} className="horizontalContainer">
+                <Portfolio/>
+                <h1>{"Which portfolio interests you?"}</h1>
+                <div className="menuContainer">
+                    <section>
+                        <h2>{"web"}</h2>
+                    </section>
+                    <section>
+                        <h2>{"game"}</h2>
+                    </section>
+                </div>
+                <Portfolio/>
+            </motion.div>
         </main>
     )
 }
