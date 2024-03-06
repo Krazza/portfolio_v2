80de5f6d0633dc7eb348f75f002f9a87b7b83749
Krazza <muondove@gmail.com>
2024-03-06 16:13:24 +0200

    layout wip

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,16 +17,19 @@ function Home () {
 
 function Greetings() {
     return(
-    <div className="greetings">
-        <div className="deets">
-            <section className="title">
-                <h3>{"Hey, I am"}</h3>
-                <h1>{"Vladislav Emerson Muondo"}</h1>
-            </section>
-            <div className="textBorder"/>
-            <p>{"A software developer based in Helsinki, Finland. Passionate about solving problems and creating useful apps. I am also a bedroom guitarist, sci-fi & fantasy reader and a video games fan."}</p>
+    <div className="container">
+        <div className="greetings">
+            <div className="deets">
+                <section className="title">
+                    <h3>{"Hey, I am"}</h3>
+                    <h1>{"Vladislav Emerson Muondo"}</h1>
+                </section>
+                <div className="textBorder"/>
+                <p>{"A software developer based in Helsinki, Finland. Passionate about solving problems and creating useful apps. I am also a bedroom guitarist, sci-fi & fantasy reader and a video games fan."}</p>
+            </div>
+            <img alt={greetPic.alt} src={greetPic.pic}/>
         </div>
-        <img alt={greetPic.alt} src={greetPic.pic}/>
+        <h2 className="tie">{"CHECK OUT MY PROJECTS"}</h2>
     </div>
     )
 }
